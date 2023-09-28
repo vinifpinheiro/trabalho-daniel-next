bf254f1f7c0f7c3e0a1963355ded50738875a6f6
VINICIUS PINHEIRO <viniciusfreitasp@hotmail.com>
2023-09-27 22:51:39 -0300

    feat: server side

--- src/pages/api/server.ts
+++ src/pages/api/server.ts
@@ -1,12 +1,12 @@
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps } from "next";
 
-export const FuncServer: GetServerSideProps = async () => {
-  const data = await fetch('https://api.example.com/data')
-  const json = await data.json()
+export const getServerProps: GetServerSideProps = async () => {
+  const data = await fetch("https://pokeapi.co/api/v2/pokemon/");
+  const json = await data.json();
 
   return {
     props: {
-      data: json
-    }
-  }
-}
+      data: json.results,
+    },
+  };
+};
